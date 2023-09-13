0db23d510b02f25ea54d434a468016d15e2205da
kbliss86 <132328278+kbliss86@users.noreply.github.com>
2023-09-13 01:13:01 -0500

    still trying to correct issue with heroku deploiymenmt - reinstalled all node mods

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,4 @@
 const router = require('express').Router();
-
-app.get('/', (req, res) => {
-    res.redirect('/home');
-});
-
 const apiRoutes = require('./api/index.js');
 const homeRoutes = require('./html/index.js');
 
